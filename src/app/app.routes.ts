7788b0ecc710f700e1f5f9645196887d6dd3c7d0
Seba-Archlinux <avila.sebastianm@gmail.com>
2024-09-16 11:54:35 -0300

    creacion de directorios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'presupuesto', loadChildren: () => import('./features/presupuesto/presupuesto.module').then(m => m.PresupuestoModule) },
+  { path: 'factura', loadChildren: () => import('./features/factura/factura.module').then(m => m.FacturaModule) },
+  { path: 'cliente', loadChildren: () => import('./features/cliente/cliente.module').then(m => m.ClienteModule) },
+  { path: '', redirectTo: '/presupuesto', pathMatch: 'full' },
+  { path: '**', redirectTo: '/presupuesto' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
