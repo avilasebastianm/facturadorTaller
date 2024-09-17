b68be26a83d602c2ad17b5f8866205ad7b7817ad
Seba-Archlinux <avila.sebastianm@gmail.com>
2024-09-17 08:07:36 -0300

    Revert "creacion de directorios"
    This reverts commit 7788b0ecc710f700e1f5f9645196887d6dd3c7d0.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,3 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'presupuesto', loadChildren: () => import('./features/presupuesto/presupuesto.module').then(m => m.PresupuestoModule) },
-  { path: 'factura', loadChildren: () => import('./features/factura/factura.module').then(m => m.FacturaModule) },
-  { path: 'cliente', loadChildren: () => import('./features/cliente/cliente.module').then(m => m.ClienteModule) },
-  { path: '', redirectTo: '/presupuesto', pathMatch: 'full' },
-  { path: '**', redirectTo: '/presupuesto' }
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [];
